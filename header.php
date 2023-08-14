72a60dcf4bdce22f3d3cbbbe318667f126e6a11d
Giuseppe Mastrandrea <74544350+giuseppemastrandrea@users.noreply.github.com>
2023-08-14 12:25:23 +0200

    web analytics italia tracking code

--- header.php
+++ header.php
@@ -19,6 +19,23 @@ $theme_locations = get_nav_menu_locations();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- START WEB ANALYTICS ITALIA TRACKING -->
+    <script type="text/javascript" src="https://ingestion.webanalytics.italia.it/matomo.js"></script>
+    <script type="text/javascript">
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="https://ingestion.webanalytics.italia.it/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', 'wBEpDRx0yL']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+    </script>
+        <!-- End Matomo Code -->
+    <!-- END WEB ANALYTICS ITALIA TRACKING -->
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
