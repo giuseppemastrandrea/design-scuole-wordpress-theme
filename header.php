b1b41534e56a4ac67f2f792d4a33c8a638a11823
Giuseppe Mastrandrea <74544350+giuseppemastrandrea@users.noreply.github.com>
2023-08-14 19:56:12 +0200

    test fix duplicated ids

--- header.php
+++ header.php
@@ -133,7 +133,7 @@ if(is_search() || is_archive())
 
                                         ?>
                                         <li class="text-purplelight menu-dropdown-simple-wrapper">
-                                            <a class="toggle-dropdown toggle-dropdown-simple <?php echo $active_page == 'service' ? 'active' : ''?>" role="button" href="#" aria-expanded="false" id="mainNavDropdown2" title="Vai alla pagina: <?php _e("Servizi","design_scuole_italia"); ?>"><?php _e("Servizi","design_scuole_italia"); ?></a>
+                                            <a class="toggle-dropdown toggle-dropdown-simple <?php echo $active_page == 'service' ? 'active' : ''?>" role="button" href="#" aria-expanded="false" id="mainNavDropdown2-main" title="Vai alla pagina: <?php _e("Servizi","design_scuole_italia"); ?>"><?php _e("Servizi","design_scuole_italia"); ?></a>
                                             <?php wp_nav_menu(array(
                                                 "menu" => $menu_obj, 
                                                 "items_wrap" => $items_wrap,
@@ -163,7 +163,7 @@ if(is_search() || is_archive())
 
                                         ?>
                                         <li class="text-greendark menu-dropdown-simple-wrapper">
-                                            <a class="toggle-dropdown toggle-dropdown-simple <?php echo $active_page == 'news' ? 'active' : ''?>" role="button" href="#" aria-expanded="false" id="mainNavDropdown3" title="Vai alla pagina: <?php _e("Novità","design_scuole_italia"); ?>"><?php _e("Novità","design_scuole_italia"); ?></a>
+                                            <a class="toggle-dropdown toggle-dropdown-simple <?php echo $active_page == 'news' ? 'active' : ''?>" role="button" href="#" aria-expanded="false" id="mainNavDropdown3-main" title="Vai alla pagina: <?php _e("Novità","design_scuole_italia"); ?>"><?php _e("Novità","design_scuole_italia"); ?></a>
                                             <?php wp_nav_menu(array(
                                                 "menu" => $menu_obj, 
                                                 "items_wrap" => $items_wrap,
@@ -192,7 +192,7 @@ if(is_search() || is_archive())
 
                                         ?>
                                         <li class="text-bluelectric menu-dropdown-simple-wrapper">
-                                            <a class="toggle-dropdown toggle-dropdown-simple <?php echo $active_page == 'education' ? 'active' : ''?>" role="button" href="#" aria-expanded="false" id="mainNavDropdown4" title="Vai alla pagina: <?php _e("Didattica","design_scuole_italia"); ?>"><?php _e("Didattica","design_scuole_italia"); ?></a>
+                                            <a class="toggle-dropdown toggle-dropdown-simple <?php echo $active_page == 'education' ? 'active' : ''?>" role="button" href="#" aria-expanded="false" id="mainNavDropdown4-main" title="Vai alla pagina: <?php _e("Didattica","design_scuole_italia"); ?>"><?php _e("Didattica","design_scuole_italia"); ?></a>
                                             <?php wp_nav_menu(array(
                                                 "menu" => $menu_obj, 
                                                 "items_wrap" => $items_wrap,
