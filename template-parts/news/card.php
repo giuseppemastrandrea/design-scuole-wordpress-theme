ee82cc32d411c3da5b97261d2d0d4660378347ec
Giuseppe Mastrandrea <74544350+giuseppemastrandrea@users.noreply.github.com>
2023-09-13 11:14:50 +0200

    style fix and date added to widget

--- template-parts/news/card.php
+++ template-parts/news/card.php
@@ -1,16 +1,21 @@
 <?php
-global $progetto;
+global $post;
 
 ?>
 
 	<div class="card card-bg card-icon rounded custom-card home-news-card mb-2">
-		<a href="<?php echo get_permalink($progetto); ?>">
-            <div class="img-wrapper" style="background-image: url(<?php echo get_the_post_thumbnail_url($progetto->ID) != null ? get_the_post_thumbnail_url($progetto->ID) : "/wp-content/uploads/2023/09/logotipo3-scaled.webp" ?>);background-size: 70%;background-repeat:no-repeat;background-position:center">
+		<a href="<?php echo get_permalink($post); ?>">
+            <div class="date">
+                <span class="year"><?php echo date_i18n("Y", strtotime($post->post_date)); ?></span>
+                <span class="day"><?php echo date_i18n("d", strtotime($post->post_date)); ?></span>
+                <span class="month"><?php echo date_i18n("M", strtotime($post->post_date)); ?></span>
+            </div>
+            <div class="img-wrapper" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID) != null ? get_the_post_thumbnail_url($post->ID) : "/wp-content/uploads/2023/09/logotipo3-scaled.webp" ?>);">
             </div>
             <div class="card-body">
-                <div class="card-icon-content" id="card-desc-<?php echo $progetto->ID; ?>">
+                <div class="card-icon-content" id="card-desc-<?php echo $post->ID; ?>">
                     <h3><strong><?php echo get_the_title($post); ?></strong></h3>
-                    <small><?php echo dsi_get_meta("sottotitolo", '_dsi_progetto_', $progetto->ID); ?></small>
+                    <small><?php echo dsi_get_meta("sottotitolo", '_dsi_progetto_', $post->ID); ?></small>
                 </div><!-- /card-icon-content -->
             </div><!-- /card-body -->
         </a>
