31f55645f0994de2fbf9818769e1354abc78077d
Giuseppe Mastrandrea <74544350+giuseppemastrandrea@users.noreply.github.com>
2025-01-11 09:47:43 +0100

    Durata progetti nascosta

--- single-scheda_progetto.php
+++ single-scheda_progetto.php
@@ -143,7 +143,9 @@ $user_can_view_post = dsi_members_can_user_view_post(get_current_user_id(), $pos
                                 <div class="row variable-gutters">
                                     <div class="col-lg-9">
                                         <h2 class="mb-2 h3" id="art-par-cosa"><?php _e("Presentazione", "design_scuole_italia"); ?></h2>
+                                        <?php /*
                                         <h3 class="mb-2 h4"><?php _e("Durata", "design_scuole_italia"); ?></h3>
+                                        */?>
                                         <p class="text-bluelectric"><strong><?php echo dsi_get_date_evento($post); ?></strong></p>
                                         <?php
                                         if(trim(get_the_content()) != "") {
